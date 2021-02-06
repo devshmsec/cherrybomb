8c1ce20146259c80a8925d4c301804db129b1d37
Vinay Verma <1mv1n4yv3rm4@gmail.com>
2021-02-06 12:37:46 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.jsx",
+	devtool: "inline-source-map",
+	target: "electron-renderer",
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							[
+								"@babel/preset-env",
+								{
+									targets: {
+										esmodules: true,
+									},
+								},
+							],
+							"@babel/preset-react",
+						],
+					},
+				},
+			},
+			{
+				test: [/\.s[ac]ss$/i, /\.css$/i],
+				use: [
+					// [style-loader](/loaders/style-loader)
+					{ loader: "style-loader" },
+					// [css-loader](/loaders/css-loader)
+					{
+						loader: "css-loader",
+						options: {
+							modules: true,
+						},
+					},
+					// [sass-loader](/loaders/sass-loader)
+					{ loader: "sass-loader" },
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	output: {
+		filename: "app.js",
+		path: path.resolve(__dirname, "build", "js"),
+	},
+};
